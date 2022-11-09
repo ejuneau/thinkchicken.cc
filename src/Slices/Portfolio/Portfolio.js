71a98eadf0559d8e7fd2ba664cb0d6d2b140208e
ejuneau <rcjuneau8@gmail.com>
2022-11-08 19:54:02 -0500

    Updated Portfolio

--- src/Slices/Portfolio/Portfolio.js
+++ src/Slices/Portfolio/Portfolio.js
@@ -5,28 +5,40 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const projects = [
     {
-    key: 1,
-    title: "Flashcards",
-    description: <p>A simple web app, as part of Codecademy's courses. Users can create topics, and add quizzes to them. Quizzes contain flashcards which are user-generated. The project was built in HTML, CSS, and Javascript using React and Redux libraries.</p>,
-    link: "https://ejuneau.github.io/CA_Flashcards/"
+        key: 1,
+        title: "Flashcards",
+        description: <p>A simple web app, as part of Codecademy's courses. Users can create topics, and add quizzes to them. Quizzes contain flashcards which are user-generated. The project was built in HTML, CSS, and Javascript using React and Redux libraries.</p>,
+        link: "https://ejuneau.github.io/CA_Flashcards/"
     },
     {
-    key: 2,
-    title: "Ravenous",
-    description: <p>A Single-Page Application (SPA) from Codecademy's courses. This project uses the Yelp API to provide a detailed list of search results based on query term, location, and sorting preferences. <br></br>Note that this project requires enabling of the CORS demo server, which can be done from <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank" rel="noreferrer">this</a> link.</p>,
-    link: "https://ejuneau.github.io/CA_Ravenous/"
+        key: 2,
+        title: "Ravenous",
+        description: <p>A Single-Page Application (SPA) from Codecademy's courses. This project uses the Yelp API to provide a detailed list of search results based on query term, location, and sorting preferences. <br></br>Note that this project requires enabling of the CORS demo server, which can be done from <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank" rel="noreferrer">this</a> link.</p>,
+        link: "https://ejuneau.github.io/CA_Ravenous/"
     },
     {
-    key: 3,
-    title: "Jammming",
-    description: <p>Another SPA built with React from Codecademy's courses. This project uses Spotify's API to allow users to search for artists, songs, and albums and build a custom playlist that is then saved to the user's Spotify Library.</p>,
-    link: "https://ejuneau.github.io/CA_Jammming/"
+        key: 3,
+        title: "Jammming",
+        description: <p>Another SPA built with React from Codecademy's courses. This project uses Spotify's API to allow users to search for artists, songs, and albums and build a custom playlist that is then saved to the user's Spotify Library.</p>,
+        link: "https://ejuneau.github.io/CA_Jammming/"
     },
     {
-    key: 4,
-    title: "Artemis for Reddit",
-    description: <p>By far my most ambitious project. This is the Capstone project for the Front-End section of Codecademy's courses. Utilizing APIs, React, and Redux, this project will be a fully functional Reddit client once it's complete. Currently in its first iterations, this is the project that I'm working on the most at the current moment. Please come revisit the project in a few days to see what progress is done.</p>,
-    link: "https://ejuneau.github.io/CA_Artemis/"
+        key: 4,
+        title: "Petlovers",
+        description: <p>A mockup of a pet adoption site, from Codecademy's courses. Users can sort through the available pets, and view specific pages (if the pet has a photo set). Uses React libraries.</p>,
+        link: "https://ejuneau.github.io/CA_Petlovers/"
+    },
+    {
+        key: 5,
+        title: "Appointment Planner",
+        description: <p>A simple contact management app, as well as a planner service. This app was made for Codecademy's code challenge, and uses React and state hooks to manage contacts and appointments.</p>,
+        link: "https://ejuneau.github.io/CA_Appointment-Planner/"
+    },
+    {
+        key: 6,
+        title: "Artemis for Reddit",
+        description: <p>By far my most ambitious project. This is the Capstone project for the Front-End section of Codecademy's courses. Utilizing APIs, React, and Redux, this project will be a fully functional Reddit client once it's complete. Currently in its first iterations, this is the project that I'm working on the most at the current moment. Please come revisit the project in a few days to see what progress is done.</p>,
+        link: "https://ejuneau.github.io/CA_Artemis/"
     }
 ]
 
@@ -48,7 +60,7 @@ class Portfolio extends React.Component {
                 </div>     
             </main>
             <div id="ProjectCarousel">
-                <CarouselProvider naturalSlideWidth={2560} naturalSlideHeight={640} totalSlides={projects.length} infinite="yes" >
+                <CarouselProvider naturalSlideWidth={1} naturalSlideHeight={1} totalSlides={projects.length} infinite="yes" >
                     <Slider>
                         {
                             projects.map(({key, title, description, link}) => {
